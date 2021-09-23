70af2e0e20c63c31a1c23384d2a8606d5e1132a6
Danillo Magno <danillo.magno@zbra.com.br>
2021-09-23 12:17:24 -0300

    remove if/else

--- Maybe.Test/MaybeSingle/MaybeSingleTests.cs
+++ Maybe.Test/MaybeSingle/MaybeSingleTests.cs
@@ -19,50 +19,115 @@ namespace Maybe.Test
             public bool ShouldThrow { get; set; }
         }
 
-        [Theory]
-        [MemberData(nameof(MaybeSingle_WithStructOrClassElements_TestData))]
-        public void MaybeSingle_NullPredicate_ShouldThrow<T>(TestData<T> testData)
+        [Fact]
+        public void MaybeSingle_NullArgument_ShouldReturnMaybeNothing()
         {
-            var (enumerableMock, enumeratorMock) = testData.Subject.GetMocks();
-
-            if (testData.Predicate == null)
-            {
-                Func<Maybe<T>> getResult = () => enumerableMock.Object.MaybeFirst(testData.Predicate);
-
-                getResult.Should().Throw<ArgumentNullException>();
-            }
-
-            enumerableMock.Verify(it => it.GetEnumerator(), Times.Never);
-            enumeratorMock.Verify(it => it.Current, Times.Never);
-            enumeratorMock.Verify(it => it.MoveNext(), Times.Never);
+            ((IEnumerable<int>)null).MaybeSingle().Should().Be(Maybe<int>.Nothing);
+            ((IEnumerable<string>)null).MaybeSingle().Should().Be(Maybe<string>.Nothing);
+            ((IEnumerable<int>)null).MaybeSingle(it => true).Should().Be(Maybe<int>.Nothing);
+            ((IEnumerable<string>)null).MaybeSingle(it => true).Should().Be(Maybe<string>.Nothing);
         }
 
         [Theory]
-        [MemberData(nameof(MaybeSingle_WithStructOrClassElements_TestData))]
-        public void MaybeSingle_WithStructOrClassElements_ShouldHaveExpectedBehavior<T>(
-            TestData<T> testData)
+        [MemberData(nameof(MaybeSingle_WithNullPredicate_TestData))]
+        public void MaybeSingle_WithNullPredicate_ShouldThrow<T>(TestData<T> testData)
         {
             var (enumerableMock, enumeratorMock) = testData.Subject.GetMocks();
 
-            Func<Maybe<T>> getResult = () => testData.Predicate == null
-                ? enumerableMock.Object.MaybeSingle()
-                : enumerableMock.Object.MaybeSingle(testData.Predicate);
+            Func<Maybe<T>> getResult = () => enumerableMock.Object.MaybeFirst(testData.Predicate);
 
-            if (testData.ShouldThrow)
-            {
-                getResult.Should().Throw<InvalidOperationException>().WithMessage("Sequence contains more than one element");
-            }
-            else
-            {
-                getResult().Should().BeEquivalentTo(testData.ExpectedResult);
-            }
+            getResult.Should().Throw<ArgumentNullException>();
 
             enumerableMock.Verify(it => it.GetEnumerator(), Times.Exactly(testData.ExpectedGetEnumeratorCalls));
             enumeratorMock.Verify(it => it.Current, Times.Exactly(testData.ExpectedCurrentCalls));
             enumeratorMock.Verify(it => it.MoveNext(), Times.Exactly(testData.ExpectedMoveNextCalls));
         }
 
-        public static IEnumerable<object[]> MaybeSingle_WithStructOrClassElements_TestData()
+        [Theory]
+        [MemberData(nameof(MaybeSingle_WithLessThanTwoElements_TestData))]
+        public void MaybeSingle_WithLessThanTwoElements_ShouldHaveExpectedBehavior<T>(
+            TestData<T> testData)
+        {
+            var (enumerableMock, enumeratorMock) = testData.Subject.GetMocks();
+
+            enumerableMock.Object.MaybeSingle().Should().BeEquivalentTo(testData.ExpectedResult);
+
+            enumerableMock.Verify(it => it.GetEnumerator(), Times.Exactly(testData.ExpectedGetEnumeratorCalls));
+            enumeratorMock.Verify(it => it.Current, Times.Exactly(testData.ExpectedCurrentCalls));
+            enumeratorMock.Verify(it => it.MoveNext(), Times.Exactly(testData.ExpectedMoveNextCalls));
+        }
+
+        [Theory]
+        [MemberData(nameof(MaybeSingle_WithLessThanTwoFilteredElements_TestData))]
+        public void MaybeSingle_WithLessThanTwoFilteredElements_ShouldHaveExpectedBehavior<T>(
+            TestData<T> testData)
+        {
+            var (enumerableMock, enumeratorMock) = testData.Subject.GetMocks();
+
+            enumerableMock.Object.MaybeSingle(testData.Predicate).Should().BeEquivalentTo(testData.ExpectedResult);
+
+            enumerableMock.Verify(it => it.GetEnumerator(), Times.Exactly(testData.ExpectedGetEnumeratorCalls));
+            enumeratorMock.Verify(it => it.Current, Times.Exactly(testData.ExpectedCurrentCalls));
+            enumeratorMock.Verify(it => it.MoveNext(), Times.Exactly(testData.ExpectedMoveNextCalls));
+        }
+
+        [Theory]
+        [MemberData(nameof(MaybeSingle_WithMoreThanOneElement_TestData))]
+        public void MaybeSingle_WithMoreThanOneElement_ShouldThrow<T>(
+            TestData<T> testData)
+        {
+            var (enumerableMock, enumeratorMock) = testData.Subject.GetMocks();
+
+            Func<Maybe<T>> getResult = () => enumerableMock.Object.MaybeSingle();
+
+            getResult.Should().Throw<InvalidOperationException>().WithMessage("Sequence contains more than one element");
+
+            enumerableMock.Verify(it => it.GetEnumerator(), Times.Exactly(testData.ExpectedGetEnumeratorCalls));
+            enumeratorMock.Verify(it => it.Current, Times.Exactly(testData.ExpectedCurrentCalls));
+            enumeratorMock.Verify(it => it.MoveNext(), Times.Exactly(testData.ExpectedMoveNextCalls));
+        }
+
+        [Theory]
+        [MemberData(nameof(MaybeSingle_WithMoreThanOneFilteredElement_TestData))]
+        public void MaybeSingle_WithMoreThanOneFilteredElement_ShouldThrow<T>(
+            TestData<T> testData)
+        {
+            var (enumerableMock, enumeratorMock) = testData.Subject.GetMocks();
+
+            Func<Maybe<T>> getResult = () => enumerableMock.Object.MaybeSingle(testData.Predicate);
+
+            getResult.Should().Throw<InvalidOperationException>().WithMessage("Sequence contains more than one element");
+
+            enumerableMock.Verify(it => it.GetEnumerator(), Times.Exactly(testData.ExpectedGetEnumeratorCalls));
+            enumeratorMock.Verify(it => it.Current, Times.Exactly(testData.ExpectedCurrentCalls));
+            enumeratorMock.Verify(it => it.MoveNext(), Times.Exactly(testData.ExpectedMoveNextCalls));
+        }
+
+        #region Test Data
+        public static IEnumerable<object[]> MaybeSingle_WithNullPredicate_TestData()
+        {
+            static object[] CreateNullPredicateTestData<T>(IEnumerable<T> subject)
+            {
+                return new object[]
+                {
+                    new TestData<T> {
+                        Subject = subject,
+                        Predicate = null,
+                        ExpectedGetEnumeratorCalls = 0,
+                        ExpectedCurrentCalls = 0,
+                        ExpectedMoveNextCalls = 0,
+                    }
+                };
+            }
+
+            yield return CreateNullPredicateTestData(new int[] { });
+            yield return CreateNullPredicateTestData(new int[] { 1 });
+            yield return CreateNullPredicateTestData(new int[] { 1, 2 });
+            yield return CreateNullPredicateTestData(new string[] { "1" });
+            yield return CreateNullPredicateTestData(new string[] { "1", "2" });
+        }
+
+        public static IEnumerable<object[]> MaybeSingle_WithLessThanTwoFilteredElements_TestData()
         {
             yield return new object[]
             {
@@ -99,51 +164,6 @@ namespace Maybe.Test
             };
             yield return new object[]
             {
-                new TestData<int> {
-                    Subject = new int[] { },
-                    Predicate = null,
-                    ExpectedGetEnumeratorCalls = 1,
-                    ExpectedCurrentCalls = 0,
-                    ExpectedMoveNextCalls = 1,
-                    ExpectedResult = Maybe<int>.Nothing,
-                }
-            };
-            yield return new object[]
-            {
-                new TestData<int> {
-                    Subject = new int[] { 1 },
-                    Predicate = null,
-                    ExpectedGetEnumeratorCalls = 1,
-                    ExpectedCurrentCalls = 1,
-                    ExpectedMoveNextCalls = 2,
-                    ExpectedResult = 1.ToMaybe(),
-                }
-            };
-            yield return new object[]
-            {
-                new TestData<int> {
-                    Subject = new int[] { 1, 2 },
-                    Predicate = null,
-                    ExpectedGetEnumeratorCalls = 1,
-                    ExpectedCurrentCalls = 1,
-                    ExpectedMoveNextCalls = 2,
-                    ShouldThrow = true,
-                }
-            };
-            yield return new object[]
-            {
-                new TestData<int> {
-                    Subject = new int[] { 1, 2, 3, 4, 5, 6, 7 },
-                    Predicate = i => i > 2,
-                    ExpectedGetEnumeratorCalls = 1,
-                    ExpectedCurrentCalls = 4,
-                    ExpectedMoveNextCalls = 4,
-                    ShouldThrow = true,
-                }
-            };
-
-            yield return new object[]
-            {
                 new TestData<string> {
                     Subject = new string[] { },
                     Predicate = i => i.Length > 1,
@@ -175,11 +195,34 @@ namespace Maybe.Test
                     ExpectedResult = "3".ToMaybe(),
                 }
             };
+        }
+
+        public static IEnumerable<object[]> MaybeSingle_WithLessThanTwoElements_TestData()
+        {
+            yield return new object[]
+            {
+                new TestData<int> {
+                    Subject = new int[] { },
+                    ExpectedGetEnumeratorCalls = 1,
+                    ExpectedCurrentCalls = 0,
+                    ExpectedMoveNextCalls = 1,
+                    ExpectedResult = Maybe<int>.Nothing,
+                }
+            };
+            yield return new object[]
+            {
+                new TestData<int> {
+                    Subject = new int[] { 1 },
+                    ExpectedGetEnumeratorCalls = 1,
+                    ExpectedCurrentCalls = 1,
+                    ExpectedMoveNextCalls = 2,
+                    ExpectedResult = 1.ToMaybe(),
+                }
+            };
             yield return new object[]
             {
                 new TestData<string> {
                     Subject = new string[] { },
-                    Predicate = null,
                     ExpectedGetEnumeratorCalls = 1,
                     ExpectedCurrentCalls = 0,
                     ExpectedMoveNextCalls = 1,
@@ -190,21 +233,48 @@ namespace Maybe.Test
             {
                 new TestData<string> {
                     Subject = new string[] { "1" },
-                    Predicate = null,
                     ExpectedGetEnumeratorCalls = 1,
                     ExpectedCurrentCalls = 1,
                     ExpectedMoveNextCalls = 2,
                     ExpectedResult = "1".ToMaybe(),
                 }
             };
+        }
+
+        public static IEnumerable<object[]> MaybeSingle_WithMoreThanOneElement_TestData()
+        {
+            yield return new object[]
+            {
+                new TestData<int> {
+                    Subject = new int[] { 1, 2 },
+                    ExpectedGetEnumeratorCalls = 1,
+                    ExpectedCurrentCalls = 1,
+                    ExpectedMoveNextCalls = 2,
+                    ShouldThrow = true,
+                }
+            };
             yield return new object[]
             {
                 new TestData<string> {
                     Subject = new string[] { "1", "2" },
-                    Predicate = null,
                     ExpectedGetEnumeratorCalls = 1,
                     ExpectedCurrentCalls = 1,
                     ExpectedMoveNextCalls = 2,
+                    ShouldThrow = true,
+                }
+            };
+        }
+
+        public static IEnumerable<object[]> MaybeSingle_WithMoreThanOneFilteredElement_TestData()
+        {
+            yield return new object[]
+            {
+                new TestData<int> {
+                    Subject = new int[] { 1, 2, 3, 4, 5, 6, 7 },
+                    Predicate = i => i > 2,
+                    ExpectedGetEnumeratorCalls = 1,
+                    ExpectedCurrentCalls = 4,
+                    ExpectedMoveNextCalls = 4,
                     ShouldThrow = true,
                 }
             };
@@ -220,14 +290,6 @@ namespace Maybe.Test
                 }
             };
         }
-
-        [Fact]
-        public void MaybeSingle_NullArgument_ShouldReturnMaybeNothing()
-        {
-            ((IEnumerable<int>)null).MaybeSingle().Should().Be(Maybe<int>.Nothing);
-            ((IEnumerable<string>)null).MaybeSingle().Should().Be(Maybe<string>.Nothing);
-            ((IEnumerable<int>)null).MaybeSingle(it => true).Should().Be(Maybe<int>.Nothing);
-            ((IEnumerable<string>)null).MaybeSingle(it => true).Should().Be(Maybe<string>.Nothing);
-        }
+        #endregion
     }
 }
