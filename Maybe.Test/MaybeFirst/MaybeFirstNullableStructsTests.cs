df14960d21686ef87f5a4db03d6efd7dc927357e
Danillo Magno <danillo.magno@zbra.com.br>
2021-09-25 20:26:01 -0300

    Removed if/else

--- Maybe.Test/MaybeFirst/MaybeFirstNullableStructsTests.cs
+++ Maybe.Test/MaybeFirst/MaybeFirstNullableStructsTests.cs
@@ -18,49 +18,182 @@ namespace Maybe.Test
             public Maybe<T> ExpectedResult { get; set; }
         }
 
-        [Theory]
-        [MemberData(nameof(MaybeFirst_WithNullableStructElements_TestData))]
-        public void MaybeFirst_NullPredicate_ShouldThrow<T>(NullableStructsTestData<T> testData)
-            where T : struct
+        [Fact]
+        public void MaybeFirst_NullArgument_ShouldReturnMaybeNothing()
         {
-            var (enumerableMock, enumeratorMock) = testData.Subject.GetMocks();
-
-            if (testData.Predicate == null)
-            {
-                Func<Maybe<T>> getResult = () => enumerableMock.Object.MaybeFirst(testData.Predicate);
-
-                getResult.Should().Throw<ArgumentNullException>();
-            }
-
-            enumerableMock.Verify(it => it.GetEnumerator(), Times.Never);
-            enumeratorMock.Verify(it => it.Current, Times.Never);
-            enumeratorMock.Verify(it => it.MoveNext(), Times.Never);
+            ((IEnumerable<int?>)null).MaybeFirst().Should().Be(Maybe<int>.Nothing);
+            ((IEnumerable<int?>)null).MaybeFirst(it => true).Should().Be(Maybe<int>.Nothing);
         }
 
         [Theory]
-        [MemberData(nameof(MaybeFirst_WithNullableStructElements_TestData))]
-        public void MaybeFirst_WithNullableStructElements_ShouldHaveExpectedBehavior<T>(
-            NullableStructsTestData<T> testData)
+        [MemberData(nameof(MaybeFirst_WithNullPredicate_TestData))]
+        public void MaybeFirst_WithNullPredicate_ShouldThrow<T>(NullableStructsTestData<T> testData)
             where T : struct
         {
             var (enumerableMock, enumeratorMock) = testData.Subject.GetMocks();
 
-            Func<Maybe<T>> getResult = () => testData.Predicate == null
-                ? enumerableMock.Object.MaybeFirst()
-                : enumerableMock.Object.MaybeFirst(testData.Predicate);
+            Func<Maybe<T>> getResult = () => enumerableMock.Object.MaybeFirst(testData.Predicate);
 
-            getResult().Should().BeEquivalentTo(testData.ExpectedResult);
+            getResult.Should().ThrowExactly<ArgumentNullException>();
 
             enumerableMock.Verify(it => it.GetEnumerator(), Times.Exactly(testData.ExpectedGetEnumeratorCalls));
             enumeratorMock.Verify(it => it.Current, Times.Exactly(testData.ExpectedCurrentCalls));
             enumeratorMock.Verify(it => it.MoveNext(), Times.Exactly(testData.ExpectedMoveNextCalls));
         }
 
-        public static IEnumerable<object[]> MaybeFirst_WithNullableStructElements_TestData()
+        [Theory]
+        [MemberData(nameof(MaybeFirst_WithZeroOrMoreElements_TestData))]
+        public void MaybeFirst__WithZeroOrMoreElements_ShouldHaveExpectedBehavior<T>(NullableStructsTestData<T> testData)
+            where T : struct
+        {
+            var (enumerableMock, enumeratorMock) = testData.Subject.GetMocks();
+
+            enumerableMock.Object.MaybeFirst().Should().BeEquivalentTo(testData.ExpectedResult);
+
+            enumerableMock.Verify(it => it.GetEnumerator(), Times.Exactly(testData.ExpectedGetEnumeratorCalls));
+            enumeratorMock.Verify(it => it.Current, Times.Exactly(testData.ExpectedCurrentCalls));
+            enumeratorMock.Verify(it => it.MoveNext(), Times.Exactly(testData.ExpectedMoveNextCalls));
+        }
+
+        [Theory]
+        [MemberData(nameof(MaybeFirst_WithZeroOrMoreFilteredElements_TestData))]
+        public void MaybeFirst_WithZeroOrMoreFilteredElements_ShouldHaveExpectedBehavior<T>(NullableStructsTestData<T> testData)
+            where T : struct
+        {
+            var (enumerableMock, enumeratorMock) = testData.Subject.GetMocks();
+
+            enumerableMock.Object.MaybeFirst(testData.Predicate).Should().BeEquivalentTo(testData.ExpectedResult);
+
+            enumerableMock.Verify(it => it.GetEnumerator(), Times.Exactly(testData.ExpectedGetEnumeratorCalls));
+            enumeratorMock.Verify(it => it.Current, Times.Exactly(testData.ExpectedCurrentCalls));
+            enumeratorMock.Verify(it => it.MoveNext(), Times.Exactly(testData.ExpectedMoveNextCalls));
+        }
+
+        #region Test Data
+        public static IEnumerable<object[]> MaybeFirst_WithNullPredicate_TestData()
+        {
+            static object[] CreateNullPredicateTestData<T>(IEnumerable<T?> subject) where T : struct
+            {
+                return new object[]
+                {
+                    new NullableStructsTestData<T>
+                    {
+                        Subject = subject,
+                        Predicate = null,
+                        ExpectedGetEnumeratorCalls = 0,
+                        ExpectedCurrentCalls = 0,
+                        ExpectedMoveNextCalls = 0,
+                    }
+                };
+            }
+
+            yield return CreateNullPredicateTestData(new int?[] { });
+            yield return CreateNullPredicateTestData(new int?[] { 1 });
+            yield return CreateNullPredicateTestData(new int?[] { null });
+            yield return CreateNullPredicateTestData(new int?[] { 1, 2 });
+            yield return CreateNullPredicateTestData(new int?[] { null, 1 });
+        }
+
+        public static IEnumerable<object[]> MaybeFirst_WithZeroOrMoreElements_TestData()
         {
             yield return new object[]
             {
-                new NullableStructsTestData<int> {
+                new NullableStructsTestData<int>
+                {
+                    Subject = new int?[] { },
+                    ExpectedGetEnumeratorCalls = 1,
+                    ExpectedCurrentCalls = 0,
+                    ExpectedMoveNextCalls = 1,
+                    ExpectedResult = Maybe<int>.Nothing,
+                }
+            };
+            yield return new object[]
+            {
+                new NullableStructsTestData<int>
+                {
+                    Subject = new int?[] { null },
+                    ExpectedGetEnumeratorCalls = 1,
+                    ExpectedCurrentCalls = 1,
+                    ExpectedMoveNextCalls = 1,
+                    ExpectedResult = Maybe<int>.Nothing,
+                }
+            };
+            yield return new object[]
+            {
+                new NullableStructsTestData<int>
+                {
+                    Subject = new int?[] { 1 },
+                    ExpectedGetEnumeratorCalls = 1,
+                    ExpectedCurrentCalls = 1,
+                    ExpectedMoveNextCalls = 1,
+                    ExpectedResult = 1.ToMaybe(),
+                }
+            };
+            yield return new object[]
+            {
+                new NullableStructsTestData<int>
+                {
+                    Subject = new int?[] { null, null },
+                    ExpectedGetEnumeratorCalls = 1,
+                    ExpectedCurrentCalls = 1,
+                    ExpectedMoveNextCalls = 1,
+                    ExpectedResult = Maybe<int>.Nothing,
+                }
+            };
+            yield return new object[]
+            {
+                new NullableStructsTestData<int>
+                {
+                    Subject = new int?[] { null, 1 },
+                    ExpectedGetEnumeratorCalls = 1,
+                    ExpectedCurrentCalls = 1,
+                    ExpectedMoveNextCalls = 1,
+                    ExpectedResult = Maybe<int>.Nothing,
+                }
+            };
+            yield return new object[]
+            {
+                new NullableStructsTestData<int>
+                {
+                    Subject = new int?[] { 1, 2 },
+                    ExpectedGetEnumeratorCalls = 1,
+                    ExpectedCurrentCalls = 1,
+                    ExpectedMoveNextCalls = 1,
+                    ExpectedResult = 1.ToMaybe(),
+                }
+            };
+        }
+
+        public static IEnumerable<object[]> MaybeFirst_WithZeroOrMoreFilteredElements_TestData()
+        {
+            yield return new object[]
+            {
+                new NullableStructsTestData<int>
+                {
+                    Subject = new int?[] { },
+                    Predicate = i => i > 1,
+                    ExpectedGetEnumeratorCalls = 1,
+                    ExpectedCurrentCalls = 0,
+                    ExpectedMoveNextCalls = 1,
+                    ExpectedResult = Maybe<int>.Nothing,
+                }
+            };
+            yield return new object[]
+            {
+                new NullableStructsTestData<int>
+                {
+                    Subject = new int?[] { 1 },
+                    Predicate = i => i > 1,
+                    ExpectedGetEnumeratorCalls = 1,
+                    ExpectedCurrentCalls = 1,
+                    ExpectedMoveNextCalls = 2,
+                    ExpectedResult = Maybe<int>.Nothing,
+                }
+            };
+            yield return new object[]
+            {
+                new NullableStructsTestData<int>
+                {
                     Subject = new int?[] { null },
                     Predicate = i => i > 1,
                     ExpectedGetEnumeratorCalls = 1,
@@ -71,73 +204,20 @@ namespace Maybe.Test
             };
             yield return new object[]
             {
-                new NullableStructsTestData<int> {
-                    Subject = new int?[] { },
-                    Predicate = i => i > 1,
+                new NullableStructsTestData<int>
+                {
+                    Subject = new int?[] { 1, null, 3, 4, 5, null, 7 },
+                    Predicate = i => i == null,
                     ExpectedGetEnumeratorCalls = 1,
-                    ExpectedCurrentCalls = 0,
-                    ExpectedMoveNextCalls = 1,
-                    ExpectedResult = Maybe<int>.Nothing,
-                }
-            };
-            yield return new object[]
-            {
-                new NullableStructsTestData<int> {
-                    Subject = new int?[] { 1 },
-                    Predicate = i => i > 1,
-                    ExpectedGetEnumeratorCalls = 1,
-                    ExpectedCurrentCalls = 1,
+                    ExpectedCurrentCalls = 2,
                     ExpectedMoveNextCalls = 2,
                     ExpectedResult = Maybe<int>.Nothing,
                 }
             };
             yield return new object[]
             {
-                new NullableStructsTestData<int> {
-                    Subject = new int?[] { 1, 2, 3, 4, 5 },
-                    Predicate = i => i == 3,
-                    ExpectedGetEnumeratorCalls = 1,
-                    ExpectedCurrentCalls = 3,
-                    ExpectedMoveNextCalls = 3,
-                    ExpectedResult = 3.ToMaybe(),
-                }
-            };
-            yield return new object[]
-            {
-                new NullableStructsTestData<int> {
-                    Subject = new int?[] { },
-                    Predicate = null,
-                    ExpectedGetEnumeratorCalls = 1,
-                    ExpectedCurrentCalls = 0,
-                    ExpectedMoveNextCalls = 1,
-                    ExpectedResult = Maybe<int>.Nothing,
-                }
-            };
-            yield return new object[]
-            {
-                new NullableStructsTestData<int> {
-                    Subject = new int?[] { 1 },
-                    Predicate = null,
-                    ExpectedGetEnumeratorCalls = 1,
-                    ExpectedCurrentCalls = 1,
-                    ExpectedMoveNextCalls = 1,
-                    ExpectedResult = 1.ToMaybe(),
-                }
-            };
-            yield return new object[]
-            {
-                new NullableStructsTestData<int> {
-                    Subject = new int?[] { 1, 2 },
-                    Predicate = null,
-                    ExpectedGetEnumeratorCalls = 1,
-                    ExpectedCurrentCalls = 1,
-                    ExpectedMoveNextCalls = 1,
-                    ExpectedResult = 1.ToMaybe(),
-                }
-            };
-            yield return new object[]
-            {
-                new NullableStructsTestData<int> {
+                new NullableStructsTestData<int>
+                {
                     Subject = new int?[] { 1, 2, 3, 4, 5, 6, 7 },
                     Predicate = i => i > 2,
                     ExpectedGetEnumeratorCalls = 1,
@@ -147,12 +227,6 @@ namespace Maybe.Test
                 }
             };
         }
-
-        [Fact]
-        public void MaybeFirst_NullArgument_ShouldReturnMaybeNothing()
-        {
-            ((IEnumerable<int?>)null).MaybeFirst().Should().Be(Maybe<int>.Nothing);
-            ((IEnumerable<int?>)null).MaybeFirst(it => true).Should().Be(Maybe<int>.Nothing);
-        }
+        #endregion
     }
 }
