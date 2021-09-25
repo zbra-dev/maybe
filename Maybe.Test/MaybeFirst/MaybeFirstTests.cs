61d7b7777b274a2383c518f29106e0bb5c9f3d33
Danillo Magno <danillo.magno@zbra.com.br>
2021-09-25 20:14:06 -0300

    Improved MaybeFirstTests

--- Maybe.Test/MaybeFirst/MaybeFirstTests.cs
+++ Maybe.Test/MaybeFirst/MaybeFirstTests.cs
@@ -18,47 +18,87 @@ namespace Maybe.Test
             public Maybe<T> ExpectedResult { get; set; }
         }
 
-        [Theory]
-        [MemberData(nameof(MaybeFirst_WithStructOrClassElements_TestData))]
-        public void MaybeFirst_NullPredicate_ShouldThrow<T>(TestData<T> testData)
+        [Fact]
+        public void MaybeFirst_NullArgument_ShouldReturnMaybeNothing()
         {
-            var (enumerableMock, enumeratorMock) = testData.Subject.GetMocks();
-
-            if (testData.Predicate == null)
-            {
-                Func<Maybe<T>> getResult = () => enumerableMock.Object.MaybeFirst(testData.Predicate);
-
-                getResult.Should().Throw<ArgumentNullException>();
-            }
-
-            enumerableMock.Verify(it => it.GetEnumerator(), Times.Never);
-            enumeratorMock.Verify(it => it.Current, Times.Never);
-            enumeratorMock.Verify(it => it.MoveNext(), Times.Never);
+            ((IEnumerable<int>)null).MaybeFirst().Should().Be(Maybe<int>.Nothing);
+            ((IEnumerable<string>)null).MaybeFirst().Should().Be(Maybe<string>.Nothing);
+            ((IEnumerable<int>)null).MaybeFirst(it => true).Should().Be(Maybe<int>.Nothing);
+            ((IEnumerable<string>)null).MaybeFirst(it => true).Should().Be(Maybe<string>.Nothing);
         }
 
         [Theory]
-        [MemberData(nameof(MaybeFirst_WithStructOrClassElements_TestData))]
-        public void MaybeFirst_WithStructOrClassElements_ShouldHaveExpectedBehavior<T>(
-            TestData<T> testData)
+        [MemberData(nameof(MaybeFirst_WithNullPredicate_TestData))]
+        public void MaybeFirst_WithNullPredicate_ShouldThrow<T>(TestData<T> testData)
         {
             var (enumerableMock, enumeratorMock) = testData.Subject.GetMocks();
 
-            Func<Maybe<T>> getResult = () => testData.Predicate == null
-                ? enumerableMock.Object.MaybeFirst()
-                : enumerableMock.Object.MaybeFirst(testData.Predicate);
+            Func<Maybe<T>> getResult = () => enumerableMock.Object.MaybeFirst(testData.Predicate);
 
-            getResult().Should().BeEquivalentTo(testData.ExpectedResult);
+            getResult.Should().ThrowExactly<ArgumentNullException>();
 
             enumerableMock.Verify(it => it.GetEnumerator(), Times.Exactly(testData.ExpectedGetEnumeratorCalls));
             enumeratorMock.Verify(it => it.Current, Times.Exactly(testData.ExpectedCurrentCalls));
             enumeratorMock.Verify(it => it.MoveNext(), Times.Exactly(testData.ExpectedMoveNextCalls));
         }
 
-        public static IEnumerable<object[]> MaybeFirst_WithStructOrClassElements_TestData()
+        [Theory]
+        [MemberData(nameof(MaybeFirst_WithZeroOrMoreElements_TestData))]
+        public void MaybeFirst_WithZeroOrMoreElements_ShouldHaveExpectedBehavior<T>(TestData<T> testData)
+        {
+            var (enumerableMock, enumeratorMock) = testData.Subject.GetMocks();
+
+            enumerableMock.Object.MaybeFirst().Should().BeEquivalentTo(testData.ExpectedResult);
+
+            enumerableMock.Verify(it => it.GetEnumerator(), Times.Exactly(testData.ExpectedGetEnumeratorCalls));
+            enumeratorMock.Verify(it => it.Current, Times.Exactly(testData.ExpectedCurrentCalls));
+            enumeratorMock.Verify(it => it.MoveNext(), Times.Exactly(testData.ExpectedMoveNextCalls));
+        }
+
+        [Theory]
+        [MemberData(nameof(MaybeFirst_WithZeroOrMoreFilteredElements_TestData))]
+        public void MaybeFirst_WithZeroOrMoreFilteredElements_ShouldHaveExpectedBehavior<T>(TestData<T> testData)
+        {
+            var (enumerableMock, enumeratorMock) = testData.Subject.GetMocks();
+
+            enumerableMock.Object.MaybeFirst(testData.Predicate).Should().BeEquivalentTo(testData.ExpectedResult);
+
+            enumerableMock.Verify(it => it.GetEnumerator(), Times.Exactly(testData.ExpectedGetEnumeratorCalls));
+            enumeratorMock.Verify(it => it.Current, Times.Exactly(testData.ExpectedCurrentCalls));
+            enumeratorMock.Verify(it => it.MoveNext(), Times.Exactly(testData.ExpectedMoveNextCalls));
+        }
+
+        #region Test Data
+        public static IEnumerable<object[]> MaybeFirst_WithNullPredicate_TestData()
+        {
+            static object[] CreateNullPredicateTestData<T>(IEnumerable<T> subject)
+            {
+                return new object[]
+                {
+                    new TestData<T>
+                    {
+                        Subject = subject,
+                        Predicate = null,
+                        ExpectedGetEnumeratorCalls = 0,
+                        ExpectedCurrentCalls = 0,
+                        ExpectedMoveNextCalls = 0,
+                    }
+                };
+            }
+
+            yield return CreateNullPredicateTestData(new int[] { });
+            yield return CreateNullPredicateTestData(new int[] { 1 });
+            yield return CreateNullPredicateTestData(new int[] { 1, 2 });
+            yield return CreateNullPredicateTestData(new string[] { "1" });
+            yield return CreateNullPredicateTestData(new string[] { "1", "2" });
+        }
+
+        public static IEnumerable<object[]> MaybeFirst_WithZeroOrMoreFilteredElements_TestData()
         {
             yield return new object[]
             {
-                new TestData<int> {
+                new TestData<int>
+                {
                     Subject = new int[] { },
                     Predicate = i => i > 1,
                     ExpectedGetEnumeratorCalls = 1,
@@ -69,7 +109,8 @@ namespace Maybe.Test
             };
             yield return new object[]
             {
-                new TestData<int> {
+                new TestData<int>
+                {
                     Subject = new int[] { 1 },
                     Predicate = i => i > 1,
                     ExpectedGetEnumeratorCalls = 1,
@@ -80,51 +121,8 @@ namespace Maybe.Test
             };
             yield return new object[]
             {
-                new TestData<int> {
-                    Subject = new int[] { 1, 2, 3, 4, 5 },
-                    Predicate = i => i == 3,
-                    ExpectedGetEnumeratorCalls = 1,
-                    ExpectedCurrentCalls = 3,
-                    ExpectedMoveNextCalls = 3,
-                    ExpectedResult = 3.ToMaybe(),
-                }
-            };
-            yield return new object[]
-            {
-                new TestData<int> {
-                    Subject = new int[] { },
-                    Predicate = null,
-                    ExpectedGetEnumeratorCalls = 1,
-                    ExpectedCurrentCalls = 0,
-                    ExpectedMoveNextCalls = 1,
-                    ExpectedResult = Maybe<int>.Nothing,
-                }
-            };
-            yield return new object[]
-            {
-                new TestData<int> {
-                    Subject = new int[] { 1 },
-                    Predicate = null,
-                    ExpectedGetEnumeratorCalls = 1,
-                    ExpectedCurrentCalls = 1,
-                    ExpectedMoveNextCalls = 1,
-                    ExpectedResult = 1.ToMaybe(),
-                }
-            };
-            yield return new object[]
-            {
-                new TestData<int> {
-                    Subject = new int[] { 1, 2 },
-                    Predicate = null,
-                    ExpectedGetEnumeratorCalls = 1,
-                    ExpectedCurrentCalls = 1,
-                    ExpectedMoveNextCalls = 1,
-                    ExpectedResult = 1.ToMaybe(),
-                }
-            };
-            yield return new object[]
-            {
-                new TestData<int> {
+                new TestData<int>
+                {
                     Subject = new int[] { 1, 2, 3, 4, 5, 6, 7 },
                     Predicate = i => i > 2,
                     ExpectedGetEnumeratorCalls = 1,
@@ -133,10 +131,10 @@ namespace Maybe.Test
                     ExpectedResult = 3.ToMaybe(),
                 }
             };
-
             yield return new object[]
             {
-                new TestData<string> {
+                new TestData<string>
+                {
                     Subject = new string[] { },
                     Predicate = i => i.Length > 1,
                     ExpectedGetEnumeratorCalls = 1,
@@ -147,7 +145,8 @@ namespace Maybe.Test
             };
             yield return new object[]
             {
-                new TestData<string> {
+                new TestData<string>
+                {
                     Subject = new string[] { "1" },
                     Predicate = i => int.Parse(i) > 1,
                     ExpectedGetEnumeratorCalls = 1,
@@ -155,54 +154,11 @@ namespace Maybe.Test
                     ExpectedMoveNextCalls = 2,
                     ExpectedResult = Maybe<string>.Nothing,
                 }
-            };
+            };        
             yield return new object[]
             {
-                new TestData<string> {
-                    Subject = new string[] { "1", "2", "3", "4", "5" },
-                    Predicate = i => int.Parse(i) == 3,
-                    ExpectedGetEnumeratorCalls = 1,
-                    ExpectedCurrentCalls = 3,
-                    ExpectedMoveNextCalls = 3,
-                    ExpectedResult = "3".ToMaybe(),
-                }
-            };
-            yield return new object[]
-            {
-                new TestData<string> {
-                    Subject = new string[] { },
-                    Predicate = null,
-                    ExpectedGetEnumeratorCalls = 1,
-                    ExpectedCurrentCalls = 0,
-                    ExpectedMoveNextCalls = 1,
-                    ExpectedResult = Maybe<string>.Nothing,
-                }
-            };
-            yield return new object[]
-            {
-                new TestData<string> {
-                    Subject = new string[] { "1" },
-                    Predicate = null,
-                    ExpectedGetEnumeratorCalls = 1,
-                    ExpectedCurrentCalls = 1,
-                    ExpectedMoveNextCalls = 1,
-                    ExpectedResult = "1".ToMaybe(),
-                }
-            };
-            yield return new object[]
-            {
-                new TestData<string> {
-                    Subject = new string[] { "1", "2" },
-                    Predicate = null,
-                    ExpectedGetEnumeratorCalls = 1,
-                    ExpectedCurrentCalls = 1,
-                    ExpectedMoveNextCalls = 1,
-                    ExpectedResult = "1".ToMaybe(),
-                }
-            };
-            yield return new object[]
-            {
-                new TestData<string> {
+                new TestData<string>
+                {
                     Subject = new string[] { "1", "2", "3", "4", "5", "6", "7" },
                     Predicate = i => int.Parse(i) > 2,
                     ExpectedGetEnumeratorCalls = 1,
@@ -213,13 +169,75 @@ namespace Maybe.Test
             };
         }
 
-        [Fact]
-        public void MaybeFirst_NullArgument_ShouldReturnMaybeNothing()
+        public static IEnumerable<object[]> MaybeFirst_WithZeroOrMoreElements_TestData()
         {
-            ((IEnumerable<int>)null).MaybeFirst().Should().Be(Maybe<int>.Nothing);
-            ((IEnumerable<string>)null).MaybeFirst().Should().Be(Maybe<string>.Nothing);
-            ((IEnumerable<int>)null).MaybeFirst(it => true).Should().Be(Maybe<int>.Nothing);
-            ((IEnumerable<string>)null).MaybeFirst(it => true).Should().Be(Maybe<string>.Nothing);
+            yield return new object[]
+            {
+                new TestData<int>
+                {
+                    Subject = new int[] { },
+                    ExpectedGetEnumeratorCalls = 1,
+                    ExpectedCurrentCalls = 0,
+                    ExpectedMoveNextCalls = 1,
+                    ExpectedResult = Maybe<int>.Nothing,
+                }
+            };
+            yield return new object[]
+            {
+                new TestData<int>
+                {
+                    Subject = new int[] { 1 },
+                    ExpectedGetEnumeratorCalls = 1,
+                    ExpectedCurrentCalls = 1,
+                    ExpectedMoveNextCalls = 1,
+                    ExpectedResult = 1.ToMaybe(),
+                }
+            };
+            yield return new object[]
+            {
+                new TestData<string>
+                {
+                    Subject = new string[] { },
+                    ExpectedGetEnumeratorCalls = 1,
+                    ExpectedCurrentCalls = 0,
+                    ExpectedMoveNextCalls = 1,
+                    ExpectedResult = Maybe<string>.Nothing,
+                }
+            };
+            yield return new object[]
+            {
+                new TestData<string>
+                {
+                    Subject = new string[] { "1" },
+                    ExpectedGetEnumeratorCalls = 1,
+                    ExpectedCurrentCalls = 1,
+                    ExpectedMoveNextCalls = 1,
+                    ExpectedResult = "1".ToMaybe(),
+                }
+            };
+            yield return new object[]
+            {
+                new TestData<int>
+                {
+                    Subject = new int[] { 1, 2 },
+                    ExpectedGetEnumeratorCalls = 1,
+                    ExpectedCurrentCalls = 1,
+                    ExpectedMoveNextCalls = 1,
+                    ExpectedResult = 1.ToMaybe(),
+                }
+            };
+            yield return new object[]
+            {
+                new TestData<string>
+                {
+                    Subject = new string[] { "1", "2" },
+                    ExpectedGetEnumeratorCalls = 1,
+                    ExpectedCurrentCalls = 1,
+                    ExpectedMoveNextCalls = 1,
+                    ExpectedResult = "1".ToMaybe(),
+                }
+            };
         }
+        #endregion
     }
 }
